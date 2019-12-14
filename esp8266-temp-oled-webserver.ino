47acae94834ec046a23fb7aef11343e2c2e60158
Mirko <pristavnik@gmail.com>
2019-12-14 01:04:01 +0100

    Update esp8266-temp-oled-webserver.ino

--- esp8266-temp-oled-webserver.ino
+++ esp8266-temp-oled-webserver.ino
@@ -8,27 +8,32 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 5
-#define OLED_SDA 13 //MOSI
-#define OLED_SCL 14 //CLK
-#define OLED_DC 2 //DC
-#define OLED_CS 12 //CS - BROJ SLOBODNOG PINA, NE SPAJA SE
+#define ONE_WIRE_BUS 5 //PIN ZA TERMOMETAR DATA
+#define OLED_SDA 13 //MOSI SDA PIN ZA OLED
+#define OLED_SCL 14 //CLK SCL PIN ZA OLED
+#define OLED_DC 2 //DC HARDWARE SPI, NE SPAJA SE
+#define OLED_CS 12 //CS HARDWARE SPI, NE SPAJA SE
 #define OLED_RESET 0 //RES
+#define RELAY 3 //PIN ZA RELEJ
 
-const char* ssid = "WIFI SSID";
-const char* password = "LOZINKA";
+const char* ssid = ""; //NAZIV WIFI MREŽE
+const char* password = ""; //LOZINKA ZA WIFI
 const long pollTempInterval = 1000; //INTERVAL CITANJA SENZORA TEMPERATURE
-String displayedTempC = "n/a";
+String displayedTempC = "n/a"; //PLACEHOLDER ZA PODATAK SA TERMOMETRA
+float temp; //PLACEHOLDER ZA DECIMALNI OBLIK PODATKA SA TERMOMETRA
 unsigned long previousMillis;
+const char* PARAM_INPUT_1 = "setpoint1"; //PLACEHOLDER ZA URL QUERY PARAMETAR 
+const char* PARAM_INPUT_2 = "setpoint2"; //PLACEHOLDER ZA URL QUERY PARAMETAR 
 
 // KONSTRUKTOR ZA OLED EKRAN
-
 Adafruit_SSD1306 display(OLED_SDA,OLED_SCL, OLED_DC, OLED_RESET, OLED_CS); 
-
+// KONSTRUKTOR ZA TERMOMETAR
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+// KONSTRUKTOR ZA HTTP SERVER
 ESP8266WebServer server(80);
 
+// SPAJANJE NA WIFI I PRIKAZ IP ADRESE
 void setupWiFi(){
   Serial.println("WIFI SPAJANJE ");
   WiFi.begin(ssid, password);
@@ -40,7 +45,7 @@ void setupWiFi(){
   Serial.println("WIFI SPOJEN ");
   Serial.println(WiFi.localIP());
 }
-
+// SOFTWARE STOPERICA
 bool updateInterval(){
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= pollTempInterval) {
@@ -50,19 +55,19 @@ bool updateInterval(){
     return false;
   }
 }
-
+// ODGOVOR NA HTTP / ZAHTJEV
 void handleRoot() {
   display.fillRect(122, 0, 5, 5, 1);
   display.display();
   server.send(200, "text/plain", "Temperatura: " + displayedTempC + (char)176 + "C");
 }
-
+// ODGOVOR NA HTTP /temp ZAHTJEV
 void handleNumberOnly() {
   display.fillRect(122, 0, 5, 5, 1);
   display.display();
   server.send(200, "text/plain", displayedTempC);
 }
-
+// 404 ODGOVOR
 void handleNotFound(){
   String message = "File Not Found\n\n";
   message += "URI: ";
@@ -77,7 +82,7 @@ void handleNotFound(){
   }
   server.send(404, "text/plain", message);
 }
-
+// PARAMETRIRANJE ISPISA NA EKRANU
 void setupDisplay(){
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
@@ -86,25 +91,28 @@ void setupDisplay(){
   display.setFont(&FreeSans24pt7b);
   display.display();  
 }
-
+// PARAMETRIRANJE ARDUINA ( POKRETANJE )
 void setup() {
   Serial.begin(115200);
   setupWiFi();
   setupDisplay();
   sensors.begin();
+  Serial.println("SENZOR POKRENUT");
   server.on("/", handleRoot);
   server.on("/temp", handleNumberOnly);
   server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("HTTP SERVER POKRENUT");
+  pinMode(RELAY, OUTPUT); //definiranje relej pina kao izlaz
+  Serial.println("RELEJ DEFINIRAN");
 }
-
+// PRIKAZ IP ADRESE NA EKRANU
 void displayIP(){
   display.setFont();
   display.setCursor(0,0);
   display.print(WiFi.localIP());
 }
-
+// PRIKAZ TEMPERATURE NA EKRANU
 void displayTemp(String celsius){
   display.clearDisplay();
   displayIP();
@@ -113,13 +121,22 @@ void displayTemp(String celsius){
   display.print(celsius);
   display.display();  
 }
-
-
+// KONSTANTNA PETLJA ( NORMALNI RAD )
 void loop() {
   server.handleClient();
   if(updateInterval()){
     sensors.requestTemperatures();
     displayedTempC = sensors.getTempCByIndex(0);
+    temp = displayedTempC.toFloat() ;
     displayTemp(displayedTempC);
+    if ( temp > 30 ){           
+    // napraviti dinamičku izmjenu pomoću url query parametara
+    digitalWrite( RELAY, HIGH);
+    }
+    if ( temp < 25 ){
+    // napraviti dinamičku izmjenu pomoću url query parametara
+    digitalWrite( RELAY, LOW);
+    }
+    // dodati još 2 setpointa
   }  
 }
